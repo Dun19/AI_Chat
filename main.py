1735ae10f517ff77e8f2ec497aeedea477cae01e
Dun19 <dun1999c@gmail.com>
2023-10-29 23:03:26 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
                                           {"role": "user", "content": "Give me 3 ideas for apps I could build with openai apis "}])
